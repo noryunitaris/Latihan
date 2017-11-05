695673b5c1fefa606e1679f9ae32ef1bc08cb3d1
noryunitaris <yunitarisnaida@gmail.com>
2017-11-05 10:21:56 -0800

    Program gaji jam kerja

--- Latihan 3.cpp
+++ Latihan 3.cpp
@@ -2,4 +2,9 @@
 using namespace std;
 
 int main(){
+	int l,hl,g,p,gt,gb,jm,jam;
+	cout<<"Program Gajih Jam Kerja"<<endl;
+	cout<<""<<endl;
+	cout<<"Berapa jam anda bekerja dalam 1 minggu : "; cin>>jam;
+	cout<<"berapa gaji anda dalam 1 minggu : "; cin>>g;
 }
