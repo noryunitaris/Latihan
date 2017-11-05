58398686925fcf879ac93e5cad3964d9c379a106
noryunitaris <yunitarisnaida@gmail.com>
2017-11-05 10:11:05 -0800

    Memilih luas yang akan dihitung

--- Latihan 2.cpp
+++ Latihan 2.cpp
@@ -2,4 +2,11 @@
 using namespace std;
 
 int main(){
+	char Pilih,P,S;
+	int a, p,lb,t,l ;
+	
+	cout<<"PROGRAM MENGHITUNG LUAS"<<endl;
+	cout<<"S = Segitiga" <<endl ;
+	cout<<"P = Persegi Panjang" <<endl;
+	cout<<"Pilih luas yang akan dihitung P atau S ? " ; cin>>Pilih;		
 }
