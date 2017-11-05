a39bd94b4af4141a7d6c1ef9013d3b413b12463f
noryunitaris <yunitarisnaida@gmail.com>
2017-11-05 10:34:14 -0800

    Mengisi Bilangan

--- Latihan 4.cpp
+++ Latihan 4.cpp
@@ -2,4 +2,11 @@
 using namespace std;
 
 int main(){
+	int a;
+	cout<<"Program Menentukan Jenis Bilangan"<<endl;
+	cout<<"`````````````````````````````````"<<endl;
+	cout<<""<<endl;
+	cout<<"masukkan bilangan : ";cin>>a;
+	cout<<""<<endl;
+	
 }
