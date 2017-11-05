fc73c3e59b9ec91a26187c78b72ebc085feaf987
noryunitaris <yunitarisnaida@gmail.com>
2017-11-05 11:23:47 -0800

    Menginput Tinggi dan Berat badan

--- Latihan 5.cpp
+++ Latihan 5.cpp
@@ -2,4 +2,11 @@
 using namespace std;
 
 int main(){
+	int t,b,h;
+	cout<<"        Using the Criteria       "<<endl;
+	cout<<"+++++++++++++++++++++++++++++++++"<<endl;
+	cout<<""<<endl;
+	cout<<"masukkan tinggi badan(cm) : "; cin>>t;
+	cout<<"masukkan berat badan(kg) : "; cin>>b;
+	cout<<""<<endl;
 }
