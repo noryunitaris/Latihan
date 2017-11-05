9229dcf36fc2506beb4d7df6f995c7734d123f72
noryunitaris <yunitarisnaida@gmail.com>
2017-11-05 09:59:11 -0800

    Menghitung selisih nilai X dan Y

--- Latihan 1.cpp
+++ Latihan 1.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main(){
+int x,y,p,selisih;
+	cout<<" masukkan bilangan x : "; cin>>x;
+	cout<<" masukkan bilangan y : "; cin>>y;
 	
+if (x>y){
+	selisih=x-y;
+	
+	cout<<"selisih Nilai X dan Y yaitu "<<selisih*1;;
+}
 }
